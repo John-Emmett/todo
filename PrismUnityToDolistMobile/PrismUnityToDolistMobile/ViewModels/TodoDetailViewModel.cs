5c2ed9663acd2346bdad8648bca5158e37dcf36e
jemmett <jemmett@DELL-DESKTOP-03>
2017-07-05 13:03:07 +0100

    Add project files.

---
+++ PrismUnityToDolistMobile/PrismUnityToDolistMobile/ViewModels/TodoDetailViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TodoList.ServiceContracts.Contracts;
+using ToDoMobileProject.DataModels.Models;
+
+namespace PrismUnityToDolistMobile.ViewModels
+{
+    public class TodoDetailViewModel : BindableBase, INavigationAware
+    {
+      
+
+        public TodoDetailViewModel()
+        {
+            
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+    }
+}
